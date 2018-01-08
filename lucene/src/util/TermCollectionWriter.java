71ffc266c7d52d6d5f6041edec372ebe4b871813
Jerome White <jerome@thewex.net>
2018-01-09 00:17:29 +0400

    Writes a collection of terms to a CSV file.

---
+++ lucene/src/util/TermCollectionWriter.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.lang.Iterable;
+import java.util.Iterator;
+import java.util.StringJoiner;
+
+public class TermCollectionWriter implements Iterator<String>,
+                                             Iterable<String> {
+    private StringJoiner header;
+    private Iterator<Term> itr;
+
+    public TermCollectionWriter(TermCollection collection) {
+        itr = collection.iterator();
+        header = new StringJoiner(",");
+        header
+            .add("name")
+            .add("ngram")
+            .add("position");
+    }
+
+    public Iterator<String> iterator() {
+        return this;
+    }
+
+    public boolean hasNext() {
+        return itr.hasNext();
+    }
+
+    public String next() {
+        String nxt;
+
+        if (header != null) {
+            nxt = header.toString();
+            header = null;
+        }
+        else {
+            nxt = itr.next().toCSV();
+        }
+
+        return nxt;
+    }
+}
