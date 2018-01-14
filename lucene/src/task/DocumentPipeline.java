a3fa1f2c5412eee24f8dcc271d5a8099d06c5f84
Jerome White <jerome@thewex.net>
2018-01-14 12:02:54 +0400

    Extraneous imports.

--- lucene/src/task/DocumentPipeline.java
+++ lucene/src/task/DocumentPipeline.java
@@ -1,25 +1,11 @@
 package task;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.concurrent.Callable;
-import java.lang.reflect.UndeclaredThrowableException;
 
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.queryparser.classic.ParseException;
-
-import util.LogAgent;
+//import util.LogAgent;
 import util.TermCollection;
 
 public class DocumentPipeline implements Callable<String> {
